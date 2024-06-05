884aca00d98b7561f0fa931783393dd53e1dac19
Daryl Dewilde <daryldewilde@gmail.com>
2024-06-05 12:17:20 +0100

    added a program and it's test script

---
+++ add.py
@@ -0,0 +1,13 @@
+def main():
+    num1 = int(input("Entrez le premier nombre"))
+    num2 = int(input("Entrez le deuxième nombre"))
+    print(num1, "+", num2, "=", add(num1, num2))
+    
+def add(*args):
+    sum = 0
+    for num  in args:
+        sum += num
+    return sum
+
+if __name__ == '__main__':
+    main()
---
+++ test.py
@@ -0,0 +1,4 @@
+import add
+
+def test_add():
+    assert(add.add(2,3) == 5)
